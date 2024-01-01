7b4f8d5bc553b87a4c1e12ba60a705befc7a5407
inspirebyte <info@inspirebyte.io>
2024-01-01 14:29:21 +0530

    fix changes

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -6,11 +6,7 @@ const Logo = ({ image, classOption }) => {
         <Link className={`${classOption}`} to={process.env.PUBLIC_URL + "/"}>
             <img
                 className="logo-main"
-<<<<<<< HEAD
-                src={image}
-=======
                 src={process.env.PUBLIC_URL + image}
->>>>>>> 8e35d25f7c30029813d9697995f8a8fbdedcdd98
                 width="70%"
                 alt="Logo"
             />
