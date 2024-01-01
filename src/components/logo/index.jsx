b60c73d3b445bc246b6a095224068dae16248911
inspirebyte <info@inspirebyte.io>
2024-01-01 20:01:10 +0530

    change width for module to 90%

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -2,25 +2,39 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const Logo = ({ image, classOption }) => {
-    return (
-        <Link className={`${classOption}`} to={process.env.PUBLIC_URL + "/"}>
-            <img
-                className="logo-main"
-                src={process.env.PUBLIC_URL + image}
-                width="65%"
-                alt="Logo"
-            />
-        </Link>
-    );
+  const mobileWidth = '90%'; // Width for mobile devices
+  const defaultWidth = '65%'; // Default width for larger screens
+
+  return (
+    <Link className={`${classOption}`} to={process.env.PUBLIC_URL + "/"}>
+      <img
+        className="logo-main"
+        src={process.env.PUBLIC_URL + image}
+        style={{
+          width: defaultWidth, // Default width for larger screens
+        }}
+        alt="Logo"
+      />
+      <style>
+        {`
+          @media (max-width: 768px) { /* Adjust the max-width to your desired breakpoint */
+            .logo-main {
+              width: ${mobileWidth} !important; // Width for mobile devices
+            }
+          }
+        `}
+      </style>
+    </Link>
+  );
 };
 
 Logo.propTypes = {
-    image: PropTypes.string,
-    classOption: PropTypes.string,
+  image: PropTypes.string,
+  classOption: PropTypes.string,
 };
 
 Logo.defaultProps = {
-    classOption: "text-center",
+  classOption: "text-center",
 };
 
 export default Logo;
