3205ccfecd58b539d8310d37953435c6a94ed1f4
inspirebyte <info@inspirebyte.io>
2024-01-05 13:33:35 +0530

    add a tag to brand-logo

--- src/components/brand/index.jsx
+++ src/components/brand/index.jsx
@@ -2,9 +2,11 @@ import PropTypes from "prop-types";
 
 const Brand = ({ data }) => {
     return (
+        <a href={data.profile} target="_blank">
         <div className="single-brand">
             <img src={process.env.PUBLIC_URL + data.image} alt="brand logo" width="50%"/>
         </div>
+        </a>
     );
 };
 
