97d649900e6c8efa739fc1ea6b1c1a98e08644fb
inspirebyte <info@inspirebyte.io>
2024-01-05 14:09:33 +0530

    fix testimony image issue

--- src/components/brand/index.jsx
+++ src/components/brand/index.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Brand = ({ data }) => {
     return (
-        <a href={data.profile} target="_blank">
+        <a href={process.env.PUBLIC_URL + data.profile} target="_blank">
         <div className="single-brand">
             <img src={process.env.PUBLIC_URL + data.image} alt="brand logo" width="50%"/>
         </div>
