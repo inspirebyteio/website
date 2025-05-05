eda199f22629bd6912a8973aa9cfd94088953b1b
inspirebyte <info@inspirebyte.io>
2025-05-05 07:50:31 +0530

    Changed some images from PNG to WEBP

--- src/containers/service/service-list/index.jsx
+++ src/containers/service/service-list/index.jsx
@@ -34,7 +34,7 @@ const ServiceListContainer = () => {
                     <div className="col-xl-4 mb-7 order-md-1 order-xl-0">
                         <div className="service-media-img text-center">
                             <img
-                                src={`${process.env.PUBLIC_URL}/images/service/inspirebyte-services.png`}
+                                src={`${process.env.PUBLIC_URL}/images/service/inspirebyte-services.webp`}
                                 alt="Inspirebyte services"
                             />
                         </div>
