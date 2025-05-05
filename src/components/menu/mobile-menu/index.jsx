070b66f50c190818d8916421f659105299995865
inspirebyte <info@inspirebyte.io>
2025-05-05 19:41:45 +0530

    remove some blog code, update inspirebyte nav logo for mobile view of the site

--- src/components/menu/mobile-menu/index.jsx
+++ src/components/menu/mobile-menu/index.jsx
@@ -39,7 +39,7 @@ const MobileMenu = ({ show, onClose }) => {
                     <div className="offcanvas-header">
                         <Logo
                             classOption="offcanvas-logo d-inline-block"
-                            image={`${process.env.PUBLIC_URL}/images/logo/logo.png`}
+                            image={`${process.env.PUBLIC_URL}/images/logo/inspirebyte-logo.png`}
                         />
                         <button
                             type="button"
